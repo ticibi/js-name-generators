fbbb399d0084897de0bd5bbd0968382088bbb4be
ticibi <tcb003@outlook.com>
2021-06-01 19:31:49 -0400

    first commit

---
+++ generator.js
@@ -0,0 +1,61 @@
+window.onload = function onLoad() {
+    generate();
+}
+
+const contentContainer = document.getElementById("content");
+const contentTitle = document.getElementById("title");
+
+var activeId = "fant";
+
+function toggleTab(value) {
+    if (value == "tech") {
+        contentTitle.innerText = "Tech Company Name Generator";
+        activeId = "tech";
+    }
+    if (value == "fant") {
+        contentTitle.innerText = "Fantasy Name Generator (f)";
+        activeId = "fant";
+    }
+    generate();
+}
+
+function choice(arr) {
+    var index = Math.floor(Math.random() * arr.length);
+    return arr[index];
+}
+
+function generateName() {
+    if (activeId === "tech") {
+        var prefix = choice(prefixes);
+        var middlefix = choice(middlefixes);
+        var suffix = choice(suffixes);
+    } else if (activeId === "fant") {
+        var prefix = choice(female_prefixes);
+        var middlefix = choice(female_middlefixes);
+        var suffix = choice(female_suffixes);
+    }
+    return prefix + middlefix + suffix;
+}
+
+function generate() {
+    remove();
+    for (i = 0; i < 7; i++) {
+        let div = document.createElement("div");
+        var name = generateName(activeId);
+        div.innerHTML = name;
+        div.id = "generated";
+        div.className = "fs-4 text-light";
+        div.style = "text-transform: capitalize";
+        contentContainer.append(div);
+    }
+}
+ 
+function remove() {
+    for (i = 0; i < 7; i++) {
+        let div = document.getElementById("generated");
+        if (div == null) {
+            return
+        }       
+        contentContainer.removeChild(div);
+    }
+}
---
+++ readme.md
@@ -0,0 +1,8 @@
+Basic name generator
+
+currently has two generators:
+
+    Tech Company Name Generator
+    Fantasy Name Generator (female)
+
+https://ticibi.github.io/name-generators.github.io/
